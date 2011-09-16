d71826a11fca985da37b1c8bf495ffa7cb33f406
Jason Staten <jstaten07@gmail.com>
2011-09-16 16:03:48 -0600

    Added Hybi14 dataframe. Not in use yet

---
+++ src/Fleck/Hybi14DataFrame.cs
@@ -0,0 +1,27 @@
+﻿namespace Fleck
+{
+    public class Hybi14DataFrame
+    {
+        public bool IsFinal { get; set; }
+
+        public Opcode Opcode { get; set; }
+
+        public bool IsMasked { get; set; }
+
+        public uint PayloadLength { get; set; }
+
+        public int MaskKey { get; set; }
+
+        public byte[] Payload { get; set; }
+    }
+
+    public enum Opcode
+    {
+        Continuation,
+        Text,
+        Binary,
+        Close = 8,
+        Ping = 9,
+        Pong = 10,
+    }
+}
