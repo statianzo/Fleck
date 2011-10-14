e9b218ab154e23d411085890774eabeb86003307
Joe Esposito <joe@joeyespo.com>
2011-10-14 01:02:14 -0400

    Added OnError to IWebSocketConnection.cs.

--- src/Fleck/Interfaces/IWebSocketConnection.cs
+++ src/Fleck/Interfaces/IWebSocketConnection.cs
@@ -7,6 +7,7 @@ namespace Fleck
         Action OnOpen { get; set; }
         Action OnClose { get; set; }
         Action<string> OnMessage { get; set; }
+        Action<Exception> OnError { get; set; }
         void Send(string message);
         void Close();
         IWebSocketConnectionInfo ConnectionInfo { get; }
