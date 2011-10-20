5b8a22fd7e554dcd39c9d1b2fdb5de261f9cad41
Jason Staten <jstaten07@gmail.com>
2011-10-20 17:59:41 -0600

    Set FleckLog to Debug.WriteLine. Cleans up command line test run

---
+++ src/Fleck.Tests/AssemblyTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace Fleck.Tests
+{
+    [SetUpFixture]
+    public class AssemblyTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            FleckLog.LogAction = (level, message, ex) => Debug.WriteLine("[{0}]{1}: {2}", level, message, ex);
+        }
+    }
+}
