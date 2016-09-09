d59f71274bfb3e134c914e00df1a6173938c8389
Michael Ikechi <michaelik@wakanow.com>
2016-09-09 15:58:04 +0100

    Added WebSocketRelay class which makes is easier to manage WebSocketConnection Lists, enables message broadcasts including or excluding the Sender from the Broadcast List.

---
+++ src/Fleck/WebSocketRelay.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fleck
+{
+    public class WebSocketRelay: List<IWebSocketConnection>
+    {
+        public string Name { get; set; }
+        public Guid ID { get; set; }
+        public long MaxLength { get; set; }
+        public static List<WebSocketRelay> Relays { get; set; }
+
+        public WebSocketRelay()
+        {
+            this.ID = Guid.NewGuid();
+            this.Name = this.ID.ToString().Substring(0, 6).ToUpper();
+            this.MaxLength = Int64.MaxValue;
+        }
+
+        public WebSocketRelay(Guid id, string name, int maxLength)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.MaxLength = maxLength;
+        }
+
+        public bool IsFull()
+        {
+            return this.LongCount() == this.MaxLength;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public bool Contains(WebSocketConnection socket)
+        {
+            if (socket == null || socket.ConnectionInfo == null) return false;
+            return Contains(socket.ConnectionInfo.Id);
+        }
+
+        public bool Contains(Guid id)
+        {
+            for (int index = 0; index <= this.Count - 1; index++)
+            {
+                if (id.Equals(this[index].ConnectionInfo.Id))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerable<IWebSocketConnection> Without(IWebSocketConnection ws)
+        {
+            if (ws == null || ws.ConnectionInfo == null) return this.AsEnumerable();
+            return this.Where((socket) => socket.ConnectionInfo.Id != ws.ConnectionInfo.Id);
+        }
+
+        public IWebSocketConnection GetSocket(Guid id)
+        {
+            return (from socket in this where socket.ConnectionInfo.Id.Equals(id) select socket).FirstOrDefault();
+        }
+
+        private void _broadcast(string Message, IEnumerable<IWebSocketConnection> recipients)
+        {
+            foreach (var socket in recipients)
+            {
+                socket.Send(Message);
+            }
+        }
+
+        private void _broadcast(byte[] Message, IEnumerable<IWebSocketConnection> recipients)
+        {
+            foreach (var socket in recipients)
+            {
+                socket.Send(Message);
+            }
+        }
+
+        private IEnumerable<IWebSocketConnection> _validateBroadcast(Guid Sender, bool IncludeSender)
+        {
+            if (this.Count == 0 | !this.Contains(Sender))
+            {
+                return null;
+            }
+            var senderSocket = GetSocket(Sender);
+            if (senderSocket == null) return null;
+            IEnumerable<IWebSocketConnection> query = this;
+            if (IncludeSender == false) query = this.Without(senderSocket);
+            return query;
+        }
+
+        private IEnumerable<IWebSocketConnection> _validateBroadcast(Guid Sender, Func<IWebSocketConnection, bool> recipientsFilter)
+        {
+            if (this.Count == 0 | !this.Contains(Sender))
+            {
+                return null;
+            }
+            var senderSocket = GetSocket(Sender);
+            if (senderSocket == null) return null;
+            IEnumerable<IWebSocketConnection> query = this.Where(recipientsFilter);
+            return query;
+        }
+
+        public bool Broadcast(string Message, Guid Sender, bool IncludeSender = false)
+        {
+            IEnumerable<IWebSocketConnection> recipients = _validateBroadcast(Sender, IncludeSender);
+            if (recipients == null) return false;
+            else
+            {
+                _broadcast(Message, recipients);
+            }
+            return true;
+        }
+
+        public bool Broadcast(byte[] Message, Guid Sender, bool IncludeSender = false)
+        {
+            IEnumerable<IWebSocketConnection> recipients = _validateBroadcast(Sender, IncludeSender);
+            if (recipients == null) return false;
+            else
+            {
+                _broadcast(Message, recipients);
+            }
+            return true;
+        }
+
+        public bool Broadcast(string Message, Guid Sender, Func<IWebSocketConnection, bool> recipientsFilter)
+        {
+            IEnumerable<IWebSocketConnection> recipients = _validateBroadcast(Sender, recipientsFilter);
+            if (recipients == null) return false;
+            else
+            {
+                _broadcast(Message, recipients);
+            }
+            return true;
+        }
+
+        public bool Broadcast(byte[] Message, Guid Sender, Func<IWebSocketConnection, bool> recipientsFilter)
+        {
+            IEnumerable<IWebSocketConnection> recipients = _validateBroadcast(Sender, recipientsFilter);
+            if (recipients == null) return false;
+            else
+            {
+                _broadcast(Message, recipients);
+            }
+            return true;
+        }
+
+        public new void Add(IWebSocketConnection ws)
+        {
+            if (!(this.Count + 1 > this.MaxLength))
+            {
+                if (ws != null && ws.ConnectionInfo != null)
+                {
+                    if (!Contains(ws)) base.Add(ws);
+                }
+                else base.Add(ws);
+            }
+        }
+    }
+}
--- src/Samples/ConsoleApp/Server.cs
+++ src/Samples/ConsoleApp/Server.cs
@@ -9,33 +9,76 @@ namespace Fleck.Samples.ConsoleApp
     {
         static void Main()
         {
+            testRelay();
+        }
+
+        static void testDefault()
+        {
             FleckLog.Level = LogLevel.Debug;
             var allSockets = new List<IWebSocketConnection>();
             var server = new WebSocketServer("ws://0.0.0.0:8181");
             server.Start(socket =>
+            {
+                socket.OnOpen = () =>
                 {
-                    socket.OnOpen = () =>
-                        {
-                            Console.WriteLine("Open!");
-                            allSockets.Add(socket);
-                        };
-                    socket.OnClose = () =>
-                        {
-                            Console.WriteLine("Close!");
-                            allSockets.Remove(socket);
-                        };
-                    socket.OnMessage = message =>
-                        {
-                            Console.WriteLine(message);
-                            allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
-                        };
-                });
-
+                    Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                    allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+                };
+            });
 
             var input = Console.ReadLine();
             while (input != "exit")
             {
                 foreach (var socket in allSockets.ToList())
+                {
+                    socket.Send(input);
+                }
+                input = Console.ReadLine();
+            }
+        }
+
+        static void testRelay()
+        {
+            FleckLog.Level = LogLevel.Debug;
+            var relay = new WebSocketRelay();
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    relay.Add(socket);
+                    socket.Send("Echo: You have Joined the Group [" + relay.Name + "]");
+                    relay.Broadcast("Relay: " + socket.ConnectionInfo.Id.ToString() + " Joined", socket.ConnectionInfo.Id);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    relay.Remove(socket);
+                    relay.Broadcast("Relay [" + socket.ConnectionInfo.Id.ToString() + "]: " + socket.ConnectionInfo.Id.ToString() + " Left", socket.ConnectionInfo.Id);
+                };
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                    socket.Send("Echo: " + message);
+                    relay.Broadcast("Relay [" + socket.ConnectionInfo.Id.ToString() + "]: " + message, socket.ConnectionInfo.Id);
+                };
+            });
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in relay.ToList())
                 {
                     socket.Send(input);
                 }
