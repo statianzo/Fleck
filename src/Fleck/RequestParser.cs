f08015a2162f60ef8c2025d3b24ac8b9e8a924ae
rosen <rosen369@outlook.com>
2018-01-11 15:22:57 +0800

    split cookie out of the body to speed up regex match time

--- src/Fleck/RequestParser.cs
+++ src/Fleck/RequestParser.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -23,6 +25,7 @@ namespace Fleck
         {
             // Check for websocket request header
             var body = Encoding.UTF8.GetString(bytes);
+            var cookie = SplitCookieWithBody(ref body);
             Match match = _regex.Match(body);
 
             if (!match.Success)
@@ -61,8 +64,43 @@ namespace Fleck
                 var value = values[i].ToString();
                 request.Headers[name] = value;
             }
-
+            if (cookie != null)
+            {
+                request.Headers["Cookie"] = cookie;
+            }
             return request;
+        }
+
+        /// <summary>
+        /// In order to prevent the cookie is too large and effect the regex match time
+        /// split out the cookie in header to speed up the match time
+        /// </summary>
+        /// <param name="body">request body</param>
+        /// <returns>cookie</returns>
+        private static string SplitCookieWithBody(ref string body)
+        {
+            string cookie = null;
+            string lastLine = null;
+            var reader = new StringReader(body);
+            var builder = new StringBuilder();
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null) break;
+                if (line.StartsWith("Cookie:"))
+                {
+                    cookie = line.Substring(8);
+                    continue;
+                }
+                builder.AppendLine(line);
+                lastLine = line;
+            }
+            body = builder.ToString();
+            if (!string.IsNullOrEmpty(lastLine))
+            {
+                body = body.Remove(body.LastIndexOf("\r"));
+            }
+            return cookie;
         }
     }
 }
